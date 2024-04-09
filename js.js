471a6edc685827bd3cd228b83bf5d7dd8aa4671d
Ulviyya Garayeva <166229990+ulviyya25@users.noreply.github.com>
2024-04-10 02:08:52 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+var button = document.querySelector('button');
+button.addEventListener('click', function() {
+    alert('Тестировщик тестирует программу, или программа тестирует тестировщика?')
+})
